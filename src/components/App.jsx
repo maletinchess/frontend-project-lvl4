711e9e16650d7b0d12ca2aea3446579e982dade9
maletinchess <maletinchess2017@gmail.com>
2022-03-04 15:09:32 +0700

    add rollbar provider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Provider as ProviderRollbar, ErrorBoundary } from '@rollbar/react';
 import {
   BrowserRouter as Router,
   Route,
@@ -108,35 +109,44 @@ const App = ({ socket }) => {
     runSocket(socket, dispatch);
   }, []);
 
+  const rollbarConfig = {
+    accessToken: '9671e3800fa04d45be0f1b34555b98d6',
+    environment: 'production',
+  };
+
   return (
-    <AuthProvider>
-      <div className="d-flex flex-column h-100">
-        <Router>
-          <Navbar bg="light" expand="lg">
-            <Link to="/">{t('header')}</Link>
-            <AuthButton />
-          </Navbar>
-          {renderModal({ modalInfo, hide: handleOnHide, socket })}
-          <Routes>
-            <Route path="/" element={<Home socket={socket} />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
-        </Router>
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </div>
-    </AuthProvider>
+    <ProviderRollbar config={rollbarConfig}>
+      <ErrorBoundary>
+        <AuthProvider>
+          <div className="d-flex flex-column h-100">
+            <Router>
+              <Navbar bg="light" expand="lg">
+                <Link to="/">{t('header')}</Link>
+                <AuthButton />
+              </Navbar>
+              {renderModal({ modalInfo, hide: handleOnHide, socket })}
+              <Routes>
+                <Route path="/" element={<Home socket={socket} />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="*" element={<NoMatch />} />
+              </Routes>
+            </Router>
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+            />
+          </div>
+        </AuthProvider>
+      </ErrorBoundary>
+    </ProviderRollbar>
   );
 };
 export default App;
