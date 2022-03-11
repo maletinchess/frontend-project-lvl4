11ce1d35089e30889039a1196d45c58d13a34ce9
maletinchess <maletinchess2017@gmail.com>
2022-03-11 12:06:16 +0700

    index, init update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import runApp from './init.jsx';
+import 'core-js/stable/index.js';
+import 'regenerator-runtime/runtime.js';
+import ReactDOM from 'react-dom';
+import init from './init.jsx';
+import '../assets/application.scss';
+
+if (process.env.NODE_ENV !== 'production') {
+  localStorage.debug = 'chat:*';
+}
+
+const runApp = async () => {
+  const vdom = await init();
+  ReactDOM.render(vdom, document.querySelector('#chat'));
+};
 
 runApp();
--- src/init.jsx
+++ src/init.jsx
@@ -1,10 +1,6 @@
 // @ts-check
 // @ts-check
-import 'core-js/stable/index.js';
-import 'regenerator-runtime/runtime.js';
 import { io } from 'socket.io-client';
-
-import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
@@ -17,10 +13,6 @@ import store from './slices/index.js';
 import '../assets/application.scss';
 
 export default async (socketApi = io()) => {
-  if (process.env.NODE_ENV !== 'production') {
-    localStorage.debug = 'chat:*';
-  }
-
   const instance = i18n.createInstance();
 
   await instance
@@ -31,13 +23,11 @@ export default async (socketApi = io()) => {
       debug: true,
     });
 
-  ReactDOM.render(
+  return (
     <Provider store={store}>
       <I18nextProvider i18n={instance}>
         <Component socket={socketApi} />
       </I18nextProvider>
-    </Provider>,
-    document.querySelector('#chat'),
+    </Provider>
   );
-  console.log('it works!');
 };
