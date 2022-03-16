1d0d84e20a57f24ba83ef02db92fd984dc7a96c5
maletinchess <maletinchess2017@gmail.com>
2022-03-16 17:39:46 +0700

    add autoscroll to messagesbox

--- src/components/MessagesBox.jsx
+++ src/components/MessagesBox.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { useSelector } from 'react-redux';
 
 const MessagesBox = () => {
   const { messages } = useSelector((state) => state.messages);
   const currentChannelId = useSelector((state) => state.channels.currentChannelId);
+
+  const filteredMessages = messages.filter((item) => item.channelId === currentChannelId);
+  const lastMessage = filteredMessages[messages.length - 1];
+
+  const messagesEndref = useRef(lastMessage);
+  const scrollToBottom = () => {
+    messagesEndref.current?.scrollIntoView();
+  };
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages, currentChannelId]);
+
   return (
-    <div className="chat-messages overflow-auto h-100 px-5">
-      {messages
-        .filter((item) => item.channelId === currentChannelId)
-        .map((item) => (
+    <div className="chat-messages overflow-auto px-5">
+      {filteredMessages
+        .map((item, index) => (
           <div
             className="text-break mb-2"
             key={item.id}
+            ref={index === filteredMessages.length - 1 ? messagesEndref : null}
           >
             <span className="mr-auto">
               <b>{item.username}</b>
