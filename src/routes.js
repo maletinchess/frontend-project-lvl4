dfccc838040da3dccb6cb7ae20b344b7fb38e4e6
maletinchess <maletinchess2017@gmail.com>
2022-03-24 13:11:21 +0700

    remove not used routes

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,6 @@ const host = '';
 const prefix = 'api/v1';
 
 export default {
-  channelsPath: () => [host, prefix, 'channels'].join('/'),
-  channelPath: (id) => [host, prefix, 'channels', id].join('/'),
-  channelMessagesPath: (id) => [host, prefix, 'channels', id, 'messages'].join('/'),
   loginPath: () => [host, prefix, 'login'].join('/'),
   usersPath: () => [host, prefix, 'data'].join('/'),
   signupPath: () => [host, prefix, 'signup'].join('/'),
