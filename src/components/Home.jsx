a6cbb94667e012a6723d412a57384011f0043335
maletinchess <maletinchess2017@gmail.com>
2022-03-26 09:27:16 +0700

    add headers to dependency-array (Home.jsx)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -43,7 +43,7 @@ const Home = ({ socket }) => {
     };
 
     fetchContent();
-  }, [location, dispatch, navigate]);
+  }, [location, dispatch, navigate, headers]);
 
   return (
     <Container className="h-100 my-4 overflow-hidden shadow rounded">
